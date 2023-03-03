7546a885a9e8b72d7c54b92c90105527fede26e4
Antonio de Pádua <antonio.junior.h@gmail.com>
2023-03-03 15:32:10 -0300

    fix: not allowing to create membership with invalid teamId

--- src/main/java/com/ecore/roles/service/implementation/MembershipsServiceImplementation.java
+++ src/main/java/com/ecore/roles/service/implementation/MembershipsServiceImplementation.java
@@ -5,6 +5,7 @@ import com.ecore.roles.exception.ResourceExistsException;
 import com.ecore.roles.exception.ResourceNotFoundException;
 import com.ecore.roles.model.Membership;
 import com.ecore.roles.model.Role;
+import com.ecore.roles.client.model.Team;
 import com.ecore.roles.repository.MembershipRepository;
 import com.ecore.roles.repository.RoleRepository;
 import com.ecore.roles.service.MembershipsService;
@@ -12,6 +13,7 @@ import lombok.NonNull;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.ecore.roles.client.TeamsClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -24,27 +26,35 @@ public class MembershipsServiceImplementation implements MembershipsService {
 
     private final MembershipRepository membershipRepository;
     private final RoleRepository roleRepository;
+    private final TeamsClient teamsClient;
 
     @Autowired
     public MembershipsServiceImplementation(
+            TeamsClient teamsClient,
             MembershipRepository membershipRepository,
             RoleRepository roleRepository) {
         this.membershipRepository = membershipRepository;
         this.roleRepository = roleRepository;
+        this.teamsClient = teamsClient;
+
     }
 
     @Override
     public Membership assignRoleToMembership(@NonNull Membership membership) {
-
+        UUID teamId = membership.getTeamId();
         UUID roleId = ofNullable(membership.getRole()).map(Role::getId)
                 .orElseThrow(() -> new InvalidArgumentException(Role.class));
 
-        if (membershipRepository.findByUserIdAndTeamId(membership.getUserId(), membership.getTeamId())
+        roleRepository.findById(roleId).orElseThrow(() -> new ResourceNotFoundException(Role.class, roleId));
+
+        if (membershipRepository.findByUserIdAndTeamId(membership.getUserId(), teamId)
                 .isPresent()) {
             throw new ResourceExistsException(Membership.class);
         }
 
-        roleRepository.findById(roleId).orElseThrow(() -> new ResourceNotFoundException(Role.class, roleId));
+        if(teamsClient.getTeam(teamId).getBody() == null) {
+            throw new ResourceNotFoundException(Team.class, teamId);
+        }
 
         return membershipRepository.save(membership);
     }
